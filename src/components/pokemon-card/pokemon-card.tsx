7e7abd3f52b3a80b0f470920d8ae3bce335ec5aa
Fabienne <fabienne_kruit@hotmail.com>
2024-03-30 22:38:44 +0100

    Add type icons

--- src/components/pokemon-card/pokemon-card.tsx
+++ src/components/pokemon-card/pokemon-card.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Pokemon } from "@/services/pokeApi";
+import { TypeIcon } from "@/components/ui/icons";
 
 export function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
   const { name, id, types, spritesFront } = pokemon;
@@ -8,15 +9,17 @@ export function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
   return (
     <li
       key={id}
-      className="rounded-sm border border-gray-200 bg-white shadow-sm"
+      className="rounded-sm border relative border-gray-200 bg-white shadow-sm"
     >
       <Link href={`/pokemon/${name}`} className="w-full h-full">
         <div className="flex flex-col items-center gap-2 p-2">
           <Image src={spritesFront} alt={name} width={140} height={140} />
           <h3>{name}</h3>
-          {types.map((type, index) => {
-            return <span key={index}>{type}</span>;
-          })}
+          <div className="absolute right-3 top-3 flex gap-2">
+            {types.map((type, index) => {
+              return <TypeIcon key={index} type={type} />;
+            })}
+          </div>
         </div>
       </Link>
     </li>
