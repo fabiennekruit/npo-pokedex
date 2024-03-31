4b904154034e98ca60af63b5ebb8c8ac8eaf1d04
Fabienne <fabienne_kruit@hotmail.com>
2024-03-31 09:56:48 +0200

    Small adjustments to styling

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -1,3 +1,4 @@
+import { BackLink } from "@/components/back-link/back-link";
 import PokemonDetail from "@/components/pokemon-detail/pokemon-detail";
 import { getPokemon } from "@/services/pokeApi";
 import { FC } from "react";
@@ -10,9 +11,10 @@ const DetailPage: FC<DetailPageProps> = async ({ params: { name } }) => {
   const pokemon = await getPokemon(name);
 
   return (
-    <>
+    <div className="relative  px-4 py-6 lg:px-24 lg:py-20">
+      <BackLink />
       <PokemonDetail pokemon={pokemon} />
-    </>
+    </div>
   );
 };
 
